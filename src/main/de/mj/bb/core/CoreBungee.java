df6c9e8788d8792d68fc4d027f2b53fde08e4335
MJGamingHDTV <binhexermueller@hotmail.de>
2018-11-03 00:00:03 +0100

    5.5.9.5 - Format code, fixed some bugs

--- src/main/de/mj/bb/core/CoreBungee.java
+++ src/main/de/mj/bb/core/CoreBungee.java
@@ -1,13 +1,9 @@
 package main.de.mj.bb.core;
 
 import lombok.Getter;
-import main.de.mj.bb.core.commands.BanCommand;
-import main.de.mj.bb.core.listener.LoginListener;
-import main.de.mj.bb.core.listener.ServerSwitchListener;
 import main.de.mj.bb.core.managers.BungeeHookManager;
-import main.de.mj.bb.core.utils.BungeeTabList;
+import main.de.mj.bb.core.managers.BungeeModuleManager;
 import main.de.mj.bb.core.utils.Data;
-import main.de.mj.bb.core.utils.FinalBan;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Listener;
@@ -21,19 +17,13 @@ public class CoreBungee extends Plugin {
     private Logger logger = this.getLogger();
     private Data data = new Data();
     private BungeeHookManager hookManager;
-    private BungeeTabList bungeeTablist;
-    private FinalBan finalBan;
+    private BungeeModuleManager moduleManager;
 
     public void onEnable() {
         hookManager = new BungeeHookManager(this);
         hookManager.hook();
+        moduleManager.init();
         ProxyServer.getInstance().registerChannel("ban");
-        new BanCommand(this);
-        new LoginListener(this);
-        bungeeTablist = new BungeeTabList(this);
-        bungeeTablist.schedule();
-        new ServerSwitchListener(this);
-        finalBan = new FinalBan(this);
     }
 
     public void registerListener(Listener listener) {
---
+++ src/main/de/mj/bb/core/managers/BungeeModuleManager.java
@@ -0,0 +1,30 @@
+package main.de.mj.bb.core.managers;
+
+import main.de.mj.bb.core.CoreBungee;
+import main.de.mj.bb.core.commands.BanCommand;
+import main.de.mj.bb.core.listener.LoginListener;
+import main.de.mj.bb.core.listener.ServerSwitchListener;
+import main.de.mj.bb.core.utils.BungeeTabList;
+import main.de.mj.bb.core.utils.Data;
+import main.de.mj.bb.core.utils.FinalBan;
+
+public class BungeeModuleManager {
+
+    private final CoreBungee coreBungee;
+    private Data data = new Data();
+    private BungeeTabList bungeeTablist;
+    private FinalBan finalBan;
+
+    public BungeeModuleManager(CoreBungee coreBungee) {
+        this.coreBungee = coreBungee;
+    }
+
+    public void init() {
+        new BanCommand(coreBungee);
+        new LoginListener(coreBungee);
+        bungeeTablist = new BungeeTabList(coreBungee);
+        bungeeTablist.schedule();
+        new ServerSwitchListener(coreBungee);
+        finalBan = new FinalBan(coreBungee);
+    }
+}
